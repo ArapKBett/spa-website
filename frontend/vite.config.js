4e43e2cfae2cab2bcdac85bd902c51754d13f26a
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-23 13:03:39 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    port: 5173,
+    port: 5173
   },
+  build: {
+    outDir: 'build' // Ensure build output matches svelte.config.js
+  }
 });
