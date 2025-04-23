c45e07d8cb5f8fdbd1fc6bb3f2a0bc91a3139ea4
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-23 11:14:48 -0400

    Create Contact.svelte

---
+++ frontend/src/pages/Contact.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ContactForm from '../components/ContactForm.svelte';
+</script>
+
+<div class="p-8">
+  <h2 class="text-2xl mb-4 text-center">Contact Us</h2>
+  <ContactForm />
+</div>
