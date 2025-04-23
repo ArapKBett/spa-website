7bc6d1452a825f7dad126e35c6bebb8d20876bf3
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-23 11:07:59 -0400

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{svelte,js,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        spaGold: '#D4AF37',
+        spaNavy: '#1A2A44',
+      },
+    },
+  },
+  plugins: [],
+};
